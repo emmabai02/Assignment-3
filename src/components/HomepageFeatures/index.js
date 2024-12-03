03c6a8b5f29ed3ee852d852c37c85e49cc4fc77b
emmabai02 <bair0102@algonquinlive.com>
2024-12-03 15:33:52 -0500

    edited homepage more

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Welcome',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Welcome to my website created for Assignment 3
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Created by',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+      Emma Baird
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Purpose',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        To demonstrate the skills learned throughout this semester.
       </>
     ),
   },
