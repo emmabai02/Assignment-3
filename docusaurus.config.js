5c3b0815c64e44f722fcfe3942ece1e010f56290
emmabai02 <bair0102@algonquinlive.com>
2024-12-03 15:24:08 -0500

    Changed links and text in the navigation bar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,19 +76,17 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Assignment 3',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+
+            to: '/docs/About', label: 'About', position: 'left'
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/docs/Contact', label: 'Contact', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
